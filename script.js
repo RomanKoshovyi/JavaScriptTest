11d2c88c5208d4c076d0eac95ae6d0f012520cb0
Roman <romanrokel@gmail.com>
2021-09-20 13:16:25 +0300

    Add form files. Start of repository work

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿function func(evt){
+    let pass1 = document.getElementById("pass1");
+    let pass2 = document.getElementById("pass2");
+    if (pass1.value != pass2.value){
+        evt.preventDefault();
+        alert("Passwords dont match! Please, try again!");
+        pass1.value = '';
+        pass2.value = '';
+    }
+    else {
+        alert ("Your registration completed successfully!")
+    }
+}
